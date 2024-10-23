16a5fdc43024e93d05153498e5138b207cbfc4ad
firoliyos <firatesfaye44@gmail.com>
2024-10-23 11:12:23 +0300

    paragraph-added

--- light-dark-mode/src/components/index.jsx
+++ light-dark-mode/src/components/index.jsx
@@ -6,6 +6,7 @@ export default function LightDarkMode() {
         <div className="light-dark-mode">
             <div className="container">
                 <p>Hello World</p>
+                <button>Change Theme</button>
             </div>
         </div>
     )
