f71708eb94f7b3b21e155b42b613ee1a8c838e6f
firoliyos <firatesfaye44@gmail.com>
2024-10-23 15:23:33 +0300

    useLocalStorage.jsx-end

--- light-dark-mode/src/components/useLocalStorage.jsx
+++ light-dark-mode/src/components/useLocalStorage.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react"
+
+export default function useLocalStorage(key, defaultValue) {
+    const [value, setValue] = useState(() => {
+        let currentValue
+       
+        try{
+           currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue))
+        }catch(error) {
+         error.message()
+         currentValue = defaultValue
+        }
+        return currentValue
+    })
+
+    useEffect(() => {
+       localStorage.setItem(key, JSON.stringify(value))
+    }, [key, value])
+
+    return [value, setValue]
+}
+
+
+
 // import { useState,useEffect } from "react"
 
 
