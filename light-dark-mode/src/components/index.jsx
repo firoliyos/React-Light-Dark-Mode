d651b94661d34a6b586118af52d08394572b9647
firoliyos <firatesfaye44@gmail.com>
2024-10-23 11:09:29 +0300

    resetup-again

--- light-dark-mode/src/components/index.jsx
+++ light-dark-mode/src/components/index.jsx
@@ -1,19 +1,35 @@
-import useLocalStorage from "./useLocalStorage"
-import './theme.css'
+import { StrictMode } from "react";
+
+
 export default function LightDarkMode() {
-
-    const [theme, setTheme] = useLocalStorage('theme', 'dark')
-    function handleToggleTheme() {
-        setTheme(theme === 'light' ? 'dark' : 'light')
-    }
-
-    console.log(theme)
     return(
-     <div className="light-dark-mode" data-theme={theme}>
-       <div className="container">
-         <p>Hello World</p>
-         <button onClick={handleToggleTheme}>Change Theme</button>
-       </div>
-     </div>   
+        <div className="light-dark-mode">
+            <div className="container">
+                Hello
+            </div>
+        </div>
     )
 }
+
+
+
+
+// import useLocalStorage from "./useLocalStorage"
+// import './theme.css'
+// export default function LightDarkMode() {
+
+//     const [theme, setTheme] = useLocalStorage('theme', 'dark')
+//     function handleToggleTheme() {
+//         setTheme(theme === 'light' ? 'dark' : 'light')
+//     }
+
+//     console.log(theme)
+//     return(
+//      <div className="light-dark-mode" data-theme={theme}>
+//        <div className="container">
+//          <p>Hello World</p>
+//          <button onClick={handleToggleTheme}>Change Theme</button>
+//        </div>
+//      </div>   
+//     )
+// }
